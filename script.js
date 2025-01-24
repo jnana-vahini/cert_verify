c6f0c4ddb7793ee1979534cf10834ab327144012
Jnana Vahini <jnanavahini221010@gmail.com>
2025-01-24 21:47:25 +0530

    Added files to the repo

---
+++ script.js
@@ -0,0 +1,14 @@
+// Load certificate data (mocking backend call)
+const certificates = [
+    { code: "ACT-NMMS-25-TR-P0Op", name: "KESHAVA NAIK", course: "NMMS Trainee Intern", date: "2025-02-01" },
+    { code: "ACT-NMMS-25-TR-XTdM", name: "HARSHITHA D", course: "NMMS Trainee Intern", date: "2025-02-01"},
+    { code: "ACT-NMMS-25-TR-ofRP", name: "DHANYASHREE", course: "NMMS Trainee Intern", date: "2025-02-01" },
+    { code: "ACT-NMMS-25-TR-E0aQ", name: "ADAMASAB JALAGERI", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/ACT-NMMS-25-TR-E0aQ.jpg" },
+    { code: "ACT-NMMS-25-TR-tTk4", name: "DITHESH GOWDA B", course: "NMMS Trainee Intern", date: "2025-02-01" },
+    { code: "ACT-NMMS-25-TR-u0TS", name: "A R SACHITH", course: "NMMS Trainee Intern", date: "2025-02-01" }
+  ];
+  
+  // Function to fetch certificate details by code
+function getCertificateDetails(certCode) {
+  return certificates.find(cert => cert.code === certCode) || null;
+}
