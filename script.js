3870d11d038f760c5437beb18c1f9e80f76acc5b
Jnana Vahini <jnanavahini221010@gmail.com>
2025-01-25 09:56:18 +0530

    Updated with all demo certificates to script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Load certificate data (mocking backend call)
 const certificates = [
-    { code: "ACT-NMMS-25-TR-P0Op", name: "KESHAVA NAIK", course: "NMMS Trainee Intern", date: "2025-02-01" },
-    { code: "ACT-NMMS-25-TR-XTdM", name: "HARSHITHA D", course: "NMMS Trainee Intern", date: "2025-02-01"},
-    { code: "ACT-NMMS-25-TR-ofRP", name: "DHANYASHREE", course: "NMMS Trainee Intern", date: "2025-02-01" },
+    { code: "ACT-NMMS-25-TR-P0Op", name: "KESHAVA NAIK", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
+    { code: "ACT-NMMS-25-TR-XTdM", name: "HARSHITHA D", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
+    { code: "ACT-NMMS-25-TR-ofRP", name: "DHANYASHREE", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
     { code: "ACT-NMMS-25-TR-E0aQ", name: "ADAMASAB JALAGERI", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
-    { code: "ACT-NMMS-25-TR-tTk4", name: "DITHESH GOWDA B", course: "NMMS Trainee Intern", date: "2025-02-01" },
-    { code: "ACT-NMMS-25-TR-u0TS", name: "A R SACHITH", course: "NMMS Trainee Intern", date: "2025-02-01" }
+    { code: "ACT-NMMS-25-TR-tTk4", name: "DITHESH GOWDA B", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
+    { code: "ACT-NMMS-25-TR-u0TS", name: "A R SACHITH", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" }
   ];
   
   // Function to fetch certificate details by code
