497be460b1dc70080562dfabd357773bb50935d3
Jnana Vahini <jnanavahini221010@gmail.com>
2025-02-01 21:48:06 +0530

    Updated certificates of interns script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Load certificate data (mocking backend call)
 const certificates = [
-    { code: "ACT-NMMS-25-TR-P0Op", name: "KESHAVA NAIK", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
-    { code: "ACT-NMMS-25-TR-XTdM", name: "HARSHITHA D", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
-    { code: "ACT-NMMS-25-TR-ofRP", name: "DHANYASHREE", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
-    { code: "ACT-NMMS-25-TR-E0aQ", name: "ADAMASAB JALAGERI", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
-    { code: "ACT-NMMS-25-TR-tTk4", name: "DITHESH GOWDA B", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/Intern_Name.png" },
+    { code: "ACT-NMMS-25-TR-P0Op", name: "KESHAVA NAIK", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/KESHAVA NAIK.jpg" },
+    { code: "ACT-NMMS-25-TR-XTdM", name: "HARSHITHA D", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/HARSHITHA D.jpg" },
+    { code: "ACT-NMMS-25-TR-ofRP", name: "DHANYASHREE", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/DHANYASHREE.jpg" },
+    { code: "ACT-NMMS-25-TR-E0aQ", name: "ADAMASAB JALAGERI", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/ADAMASAB JALAGERI.jpg" },
+    { code: "ACT-NMMS-25-TR-tTk4", name: "DITHESH GOWDA B", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/DITHESH GOWDA B.jpg" },
     { code: "ACT-NMMS-25-TR-u0TS", name: "A R SACHITH", course: "NMMS Trainee Intern", date: "2025-02-01", "image": "Certificates/A R SACHITH.jpg" }
   ];
   
